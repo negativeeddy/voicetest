dce16719d95810b8b97ffc09dc2d7babf4605d73
Ben Williams <benwilli@microsoft.com>
2020-01-09 10:23:03 -0600

    update script comments and user messages

--- deployment/createSpeechApp.ps1
+++ deployment/createSpeechApp.ps1
@@ -53,7 +53,6 @@ try {
 
 $appId = $response.appId
 write-host "Created project Id $appId"
-write-host $response
 
 #
 # update the dialog model of the app
@@ -67,7 +66,7 @@ try {
 } catch {
     # Dig into the exception to get the Response details.
     # Note that value__ is not a typo.
-    Write-Host $model
+    Write-Host $_.Exception
     Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
     Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
     exit
@@ -78,14 +77,17 @@ $model | ConvertTo-Json -depth 100 | Out-File "initialModel.json"
 
 write-host "patching the $speechAppName inventory commands model"
 
-# change the model
+# change the model based on the local json file
+
 $newModel = Get-Content './inventoryManagement.json' | Out-String | ConvertFrom-Json
 $model.httpEndpoints = $newModel.httpEndpoints
 $model.httpEndpoints[0].url = "https://$siteName.azurewebsites.net/api/Inventory/UpdateInventory"
 $model.lgTemplates = $newModel.lgTemplates
 $model.globalParameters = $newModel.globalParameters
 $model.commands = $newModel.commands
-$model | ConvertTo-Json -depth 100 | Out-File "newModel.json"
+#$model | ConvertTo-Json -depth 100 | Out-File "newModel.json"
+
+# send the updated model up to the application
 
 write-host "updating $speechAppName with the new inventory commands model"
 try {
@@ -98,20 +100,27 @@ try {
     Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
     exit
 }
-write-host "done."
+write-host "...model update completed"
 
-#start the training for the model
+#
+# start the training for the model
+#
+
 write-host "starting the training"
 $response = invoke-restmethod -Method POST -Uri "https://westus2.commands.speech.microsoft.com/apps/$appId/stages/default/cultures/en-us/train" -Header $headers
 $versionId = $response.versionId
 write-host -NoNewline "training version $versionId"
 
-#wait until the training is complete
+#
+# wait until the training is complete
+#
+
 try {
     $response = invoke-restmethod -Method GET -Uri "https://westus2.commands.speech.microsoft.com/apps/$appId/stages/default/cultures/en-us/train/$versionId" -Header $headers
 } catch {
     # Dig into the exception to get the Response details.
     # Note that value__ is not a typo.
+    Write-Host $_.Exception
     Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
     Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
     exit
@@ -127,23 +136,32 @@ while($response.completed -ne "true")
     catch {
         # Dig into the exception to get the Response details.
         # Note that value__ is not a typo.
-        Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
+    Write-Host $_.Exception
+    Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
         Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
         exit
     }
 }
-write-host "training is completed"
+write-host
+write-host "...training is completed"
 
-#publish the model
+#
+# publish the model
+#
+
 write-host "publishing the model"
 $response = invoke-restmethod -Method POST -Uri "https://westus2.commands.speech.microsoft.com/apps/$appId/stages/default/cultures/en-us/publish/$versionId" -Header $headers
-write-host "model is published"
+write-host "...model is published"
 
-#print out the relevant info for the user to put in the application
+#
+# print out the relevant info for the user to put in the application
+#
+
 Write-Host
 Write-Host "***********************"
 Write-Host "Speech commands have been published."
-Write-Host "Update these parameters in the client"
+
+Write-Host "Update these parameters in the client to use them"
 Write-Host "    SpeechApplicationId   = $appId"
 Write-Host "    SpeechSubscriptionKey = $speechResourceKey"
 Write-Host "***********************"
